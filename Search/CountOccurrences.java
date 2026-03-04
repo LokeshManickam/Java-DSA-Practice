5806ace81adf4031e1277cc34eb93cf8ee268931
LokeshManickam <LOKESHMANICKAM2005@GMAIL.COM>
2026-03-04 19:22:17 +0530

    Day 06 - Search: Implemented count occurrences using first and last binary search

---
+++ Search/CountOccurrences.java
@@ -0,0 +1,61 @@
+package Search;
+
+import java.util.Scanner;
+
+public class CountOccurrences {
+
+    public static int firstOccurrence(int[] arr, target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                result = mid;
+                right = mid - 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    public static int lastOccurrence(int[] arr, target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                result = mid;
+                left = mid + 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        int first = firstOccurrence(arr, target);
+        int last = lastOccurrence(arr, target);
+        int count = 0;
+        if (first != 0) {
+            count = last - first + 1;
+        }
+        System.out.println("Count: " + count);
+
+        sc.close();
+    }
+}
