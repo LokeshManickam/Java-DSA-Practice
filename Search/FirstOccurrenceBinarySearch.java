cdddaf79288a1ea2f8c48c77117584417b609cc6
LokeshManickam <LOKESHMANICKAM2005@GMAIL.COM>
2026-03-04 17:58:26 +0530

    Day 06 - Search: Implemented binary search to find first occurrence of element

---
+++ Search/FirstOccurrenceBinarySearch.java
@@ -0,0 +1,31 @@
+package Search;
+
+import java.util.Scanner;
+
+public class FirstOccurrenceBinarySearch {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        int left = 0;
+        int right = n - 1;
+        int result = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                result = mid;
+                right = mid - 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println("First Occurrence Index: " + result);
+    }
+}
