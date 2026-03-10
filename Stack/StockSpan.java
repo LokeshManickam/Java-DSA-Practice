6ee76709c92ad7fe2f863170244e8bd39f61296b
LokeshManickam <LOKESHMANICKAM2005@GMAIL.COM>
2026-03-10 16:27:50 +0530

    Day 09 - Stack: Implemented stock span problem using monotonic stack

---
+++ Stack/StockSpan.java
@@ -0,0 +1,33 @@
+package Stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class StockSpan {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] price = new int[n];
+        int[] span = new int[n];
+        for (int i = 0; i < n; i++) {
+            price[i] = sc.nextInt();
+        }
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && price[stack.peek()] <= price[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                span[i] = i + 1;
+            } else {
+                span[i] = i - stack.peek();
+            }
+            stack.push(i);
+        }
+        for (int s : span) {
+            System.out.println(s + " ");
+        }
+        sc.close();
+    }
+}
